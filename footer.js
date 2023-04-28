8503246e0fd2c16b0c2ceaaf737a9bc41d916a82
Reehum <reehum.farrukh67@gmail.com>
2023-04-28 16:22:12 +0500

    first commit

---
+++ footer.js
@@ -0,0 +1,49 @@
+
+document.getElementById("footer").innerHTML = `
+  <div class="row pt-4">
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>HELP</h5></li>
+        <li>0309 5552608</li>
+        <li><a href="#">Customer Service</a></li>
+        <li><a href="#">Shipping & Returns</a></li>
+        <li>socials</li>
+        <li><a  href="customer.html">Customer Dashboard</a></li>
+        <li><a  href="admin.html">Admin Dashboard </a></li>
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>ACCOUNT</h5></li>
+        <li>Your Order</li>
+        <li>Status</li>
+      </ul>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-3 foot">
+      <ul>
+        <li><h5>ABOUT</h5></li>
+        <li><a href="AboutUs.html">About Us</a></li>
+        <li><a href="Contact.html"></a>Contact</li>
+        <li><a href="team.html"></a>Team</li>
+        
+      </ul>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12  foot">
+      <ul>
+        <li><h5>Stay Updated</h5></li>
+        <li><span>info@makeupmania.com.pk</span></li>
+        <li> Khayaban-e-Firdousi, Block R1 Block R 1 Phase 2 Johar Town, Lahore, Punjab</li>
+      </ul>
+    </div>
+    <hr>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-12  foot">
+  <ul>
+  
+ 
+<div class="row mt-2">
+<div class="col-lg-12">
+  <p>Copyright © 2023 Makeup Mania Corporation Limited . All Rights Reserved</p>
+</div>
+</div>
+`
---
+++ header.js
@@ -0,0 +1,89 @@
+document.getElementById("header").innerHTML =
+   `<link rel="stylesheet" href="style.css">
+
+<header class="dark">
+<!-- navigation -->
+
+<div class="container-fluid navigation-bg">
+<div class="container">
+<nav class="navbar navbar-expand-lg navigation-bg">
+<div class="container-fluid">
+ <div class="mx-4">
+ <img src="images/logo.webp" alt="" srcset="" />
+ </div>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarScroll"
+    aria-controls="navbarScroll"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse ml-4" id="navbarScroll">
+    <ul
+      class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+      style="--bs-scroll-height: 100px"
+    >
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="index.html"
+          >Home</a
+        >
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="product.html">Products</a>
+      
+  </li>
+  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="product.html" 
+          id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" 
+          aria-expanded="false">
+          Products
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <div >
+          <h6 class="sub-menu-head">Product Categories </h6>
+            <li><a class="dropdown-item" href="#">Face</a></li>
+            <li><a class="dropdown-item" href="#">Eyes</a></li>
+            <li><a class="dropdown-item" href="#">Eyebrows</a></li>
+            <li><a class="dropdown-item" href="#">Lips</a></li>
+          </div>
+           <div>
+           <h5 class="sub-menu-head">Face</h5>
+           <li><a class="dropdown-item" href="#"> lipsticks</a></li>
+           <li><a class="dropdown-item" href="#"> EyeShadows</a></li>
+           <li><a class="dropdown-item" href="#"> Eyebrows</a></li>
+           <li><a class="dropdown-item" href="#"> Blush</a></li>
+           </div>
+          </ul>
+        </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="AboutUs.html">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.html">Contact</a>
+      </li>
+      <li class="nav-item">
+      <form class="d-flex">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search Product"
+        aria-label="Search"
+      />
+      <button class="btn btn-color btn-outline-dark" type="submit">
+        Search
+      </button>
+    </form>
+    </li>
+    </ul>
+   
+  </div>
+</div>
+</nav>
+    </div>
+</div> 
+</header>
+ 
+` 
